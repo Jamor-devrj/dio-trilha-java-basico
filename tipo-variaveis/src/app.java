e98cb93913e075835a91b7682b866d7412023b77
jamor <jamor.devbr@gmail.com>
2022-10-13 17:05:42 -0300

    teste

--- tipo-variaveis/src/app.java
+++ tipo-variaveis/src/app.java
@@ -1,5 +1,5 @@
-public class java {
+public class app {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("BOM DIA");
     }
 }
