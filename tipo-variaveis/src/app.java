4295e053b29114836bdea4fd62d479749a2a408d
jamor <jamor.devbr@gmail.com>
2022-10-13 21:01:28 -0300

    hoje

--- tipo-variaveis/src/app.java
+++ tipo-variaveis/src/app.java
@@ -1,5 +1,10 @@
 public class app {
     public static void main(String[] args) throws Exception {
+        
+        int x = 10 * 2;
+
+        
         System.out.println("BOM DIA");
+        System.out.println(x);
     }
 }
